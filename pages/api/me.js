9a45f9d344b2bd648d704ee2cebdd63abe3d4ba0
Fluke <hunterx52110@gmail.com>
2022-09-22 14:06:31 +0700

    test

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  return res.json({ name: "Thatthana", studentId: "640612088" });
+  return res.json({ name: "Thatthana Sringeon", studentId: "640612088" });
 }
